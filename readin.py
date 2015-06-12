409c3b9fb92d9b03e0bf9e0e89c37b512af71b3d
Tayeb Zaidi <zaidi@linux27-7.tuc.noao.edu>
2015-06-12 11:08:37 -0700

    Please enter the commit message for your changes. Lines starting

--- readin.py
+++ readin.py
@@ -10,6 +10,8 @@ def readin_aavso(filename):
     banddata = data[ind]
     return banddata
 
+def hstack2(arrays):
+    return arrays[0].__array_wrap__(np.hstack(arrays))
 
 def readin_SNANA(filename):
     #data = 
@@ -22,12 +24,19 @@ def readin_SNANA(filename):
 
 def readin_SNrest(filename):
     path = "/Users/zaidi/Documents/REU/restframe/"
-    #for filename in os.listdir(path):
-    current_file = os.path.join(path, filename)
     formatcode = ('|S16,'.rstrip('#') +'f8,'*6 + '|S16,' + 4 * 'f8,' + '|S16,' * 3 + 'f8,' * 2 + '|S16,' + 'f8,' * 2)
-    data = np.recfromtxt(current_file,usecols = (0,1,2,3,4,5,6), dtype = formatcode, names = True, skip_header = 13, case_sensitive = 'lower', invalid_raise = False)
-    ind = np.where((data.band == 'V'))
+    data = np.recfromtxt(os.path.join(path, filename),usecols = (0,1,2,3,4), dtype = formatcode, names = True, skip_header = 13, case_sensitive = 'lower', invalid_raise = False)
+    ind = np.where(np.logical_or(data.band == 'B', data.band == 'V') & (data.err < 0.2))
     banddata = data[ind]
+    '''
+    for filename in os.listdir(path):
+        current_file = os.path.join(path, filename)
+        dat = np.recfromtxt(current_file,usecols = (0,1,2,3,4,5,6),names=True, dtype = formatcode, skip_header = 13, case_sensitive = 'lower', invalid_raise = False)
+        ind = np.where((dat.band == 'V') & (dat.err <= 0.2 ))
+        dat.mag = dat.mag - ( np.amin(dat.mag)-11.444)
+        dat = dat[ind]
+        data = hstack2((data,dat))
+    '''
     return banddata
 
 if __name__=='__main__':
